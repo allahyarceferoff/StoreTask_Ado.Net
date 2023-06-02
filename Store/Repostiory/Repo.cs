154326574b88893c777eda59f2b5568401b69b0e
Ataki_pw11 <Ataki_pw11@STHQ0127-04>
2023-06-02 15:56:55 +0400

    Database push

---
+++ Store/Repostiory/Repo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Store.Models;
+using System.Configuration;
+
+namespace Store.Repostiory
+{
+    public class Repo
+    {
+        ObservableCollection<Product> _products = new ObservableCollection<Product>();
+        SqlConnection conn;
+        string cs = ConfigurationManager.ConnectionStrings["myConn"].ConnectionString;
+
+        public Repo()
+        {
+            using (conn = new SqlConnection())
+            {
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                SqlDataReader reader = null;
+
+                string query = "SELECT * FROM Products";
+                using (var command = new SqlCommand(query, conn))
+                {
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Product product = new Product
+                        {
+                            Id = int.Parse(reader[0].ToString()),
+                            Name = reader[1].ToString(),
+
+                        };
+                        
+                    }
+                }
+            }
+        }
+
+        public ObservableCollection<Author> GetAll()
+        {
+            return _authors;
+        }
+
+        public void Insert(int id, string firstName, string lastName)
+        {
+            using (var conn = new SqlConnection())
+            {
+                conn.ConnectionString = cs;
+                conn.Open();
+
+
+                string query = $@" INSERT INTO Authors(Id,FirstName,LastName)
+                                VALUES(@id,@firstName,@lastName)";
+
+                var paramId = new SqlParameter();
+                paramId.ParameterName = "@id";
+                paramId.SqlDbType = SqlDbType.Int;
+                paramId.Value = id;
+
+                var paramName = new SqlParameter();
+                paramName.ParameterName = "@firstName";
+                paramName.SqlDbType = SqlDbType.NVarChar;
+                paramName.Value = firstName;
+
+                var paramSurname = new SqlParameter();
+                paramSurname.ParameterName = "@lastName";
+                paramSurname.SqlDbType = SqlDbType.NVarChar;
+                paramSurname.Value = lastName;
+
+                using (var command = new SqlCommand(query, conn))
+                {
+                    command.Parameters.Add(paramId);
+                    command.Parameters.Add(paramName);
+                    command.Parameters.Add(paramSurname);
+
+                    var result = command.ExecuteNonQuery();
+                }
+                Author author = new Author
+                {
+                    Id = id,
+                    FirstName = firstName,
+                    LastName = lastName
+                };
+                _authors.Add(author);
+            }
+        }
+    }
+}
---
+++ Store/ViewModels/UCViewModels/ProductsViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Store.ViewModels.BaseViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.ViewModels.UCViewModels
+{
+    public class ProductsViewModel: BaseViewModels
+    {
+		private string productName;
+
+		public string ProductName
+		{
+			get { return productName; }
+			set { productName = value; OnPropertyChanged(); }
+		}
+
+		private int productQuantity;
+
+		public int ProductQuantity
+        {
+			get { return productQuantity; }
+			set { productQuantity = value; OnPropertyChanged(); }
+		}
+
+		private decimal productPrice;
+
+		public decimal ProductPrice
+        {
+			get { return productPrice; }
+			set { productPrice = value; OnPropertyChanged(); }
+		}
+
+		private string imagePath;
+
+		public string ImagePath
+		{
+			get { return imagePath; }
+			set { imagePath = value; OnPropertyChanged(); }
+		}
+
+		public ProductsViewModel()
+		{
+			
+		}
+
+
+
+	}
+}
